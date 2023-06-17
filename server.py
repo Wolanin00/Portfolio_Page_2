94cf2ec4f773f52b04fc0c9a14ad091ab965b5c7
Mateusz Szewczyk 1 <szewczykmateusz64@gmail.com>
2023-06-17 14:35:44 +0200

    add EXPERIENCE AND SCIENCE segment, add cv page, add download cv

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_file
 
 app = Flask(__name__)
 
@@ -6,6 +6,16 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("index.html")
+
+
+@app.route("/cv")
+def open_cv_page():
+    return send_file(path_or_file='static/file/Szewczyk_Mateusz_CV.pdf', as_attachment=False)
+
+
+@app.route("/download_cv")
+def download_cv():
+    return send_file(path_or_file='static/file/Szewczyk_Mateusz_CV.pdf', as_attachment=True)
 
 
 if __name__ == "__main__":
